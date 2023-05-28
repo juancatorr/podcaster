415d6090fc18297d0f3b9b31364cea9f0f005fc6
juancarlosclavijo <94992352+juancarlosclavijo@users.noreply.github.com>
2023-05-28 20:52:54 -0300

    component was refactored

--- src/components/PodcastItem/index.tsx
+++ src/components/PodcastItem/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Podcast } from "../../utils/Podcast";
 interface PodcastItemProps {
   podcast: Podcast;
+  isShortVersion?: boolean
 }
 
-export const PodcastItem = ({podcast}:PodcastItemProps ) => {  
-  return (
-    <div className=" relative w-60 h-60 flex flex-col justify-end">
+export const PodcastItem = ({podcast,isShortVersion=true}:PodcastItemProps ) => {  
+  if(isShortVersion){
+    return (
+      <div className=" relative w-60 h-60 flex flex-col justify-end">
       <img className=" absolute right-1/2 translate-x-1/2 rounded-full top-1/3 -translate-y-1/2 w-36 h-36" src={podcast.image} alt={podcast.title}></img>
       <div className=" flex flex-col justify-end items-center border bg-zinc-50 shadow-md h-2/3 rounded-md p-5 text-center" >
         <h1 className=" text-sm font-bold" >{`${podcast.name}`}</h1>
@@ -14,4 +16,18 @@ export const PodcastItem = ({podcast}:PodcastItemProps ) => {
       </div>
     </div>
   );
+}
+return (
+  <div className="w-60 flex flex-col bg-zinc-50 shadow-md rounded-sm p-5 text-center items-center border">
+  <img className="w-36 h-36 rounded-sm" src={podcast.image} alt={podcast.title}></img>  
+   <hr className="w-full my-2"/>
+  <div className="flex flex-col items-start text-start">
+    <h1 className="text-sm font-bold" >{`${podcast.name}`}</h1>
+    <p className="text-sm italic">{`by: ${podcast.author}`}</p>
+    <hr className="w-full my-2"/>
+    <h2 className="text-xs font-bold">Description:</h2>
+    <p className="text-xs italic">{podcast.summary}</p>
+   </div>
+</div>
+);
 }
