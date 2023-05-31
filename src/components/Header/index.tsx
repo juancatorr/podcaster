baa61b53c6d920a1399b0c71d1927f9892804e74
juancarlosclavijo <94992352+juancarlosclavijo@users.noreply.github.com>
2023-05-31 20:42:15 -0300

    set max height

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ export const Header = ({isHidden=false}) => {
   return (
     <div className={`flex flex-col items-center w-full gap-4 font-chakra`}>
         <div className={` flex flex-row justify-between items-center pl-2 text-xl font-bold w-7/12 h-8 mt-10 shadow-md text-primary-300 bg-zinc-50 `}>
-          <Link to={'/'}><p>Podcaster 🎧</p></Link>
+          <Link to={'/'}><p>Podcaster</p></Link>
           <div className={`bg-primary-300 w-5 h-5 rounded-full animate-bounce mr-9 ${isHidden && 'hidden'}`}></div>
         </div>
     </div>
--- src/components/PodcastItem/index.tsx
+++ src/components/PodcastItem/index.tsx
@@ -11,8 +11,8 @@ export const PodcastItem = ({podcast,isShortVersion=true}:PodcastItemProps ) => 
     return (
       <div className=" relative w-60 h-60 flex flex-col justify-end cursor-pointer">
       <img className=" absolute right-1/2 translate-x-1/2 rounded-full top-1/3 -translate-y-1/2 w-36 h-36" src={podcast.image} alt={podcast.title}></img>
-      <div className=" flex flex-col justify-end items-center border bg-zinc-50 shadow-md h-2/3 rounded-md p-5 text-center" >
-        <h1 className=" text-sm font-bold" >{`${podcast.name}`}</h1>
+      <div className="flex flex-col justify-end items-center border bg-zinc-50 shadow-md h-2/3 rounded-md p-5 text-center" >
+        <h1 className=" text-sm font-bold max-h-10 overflow-hidden" >{`${podcast.name}`}</h1>
         <h2 className=" text-sm text-gray-500">{`Author: ${podcast.author}`}</h2> 
       </div>
     </div>
